c9a7e8a1b50b4834d6c3c3e33f584c9fcc900791
Carlos Arriaga <37526095+TheStrechh@users.noreply.github.com>
2018-03-29 13:14:11 -0600

    Update build-rom.sh

--- build-rom.sh
+++ build-rom.sh
@@ -14,18 +14,9 @@ CLR_BLD_CYA=$CLR_RST$CLR_BLD$(tput setaf 6) #  cyan, bold
 
 device="$1"
 
-# Pick the default thread count (allow overrides from the environment)
-if [ -z "$THREADS" ]; then
-        if [ "$(uname -s)" = 'Darwin' ]; then
-                export THREADS=$(sysctl -n machdep.cpu.core_count)
-        else
-                export THREADS=$(cat /proc/cpuinfo | grep '^processor' | wc -l)
-        fi
-fi
-
 # idk only exports
-export KBUILD_BUILD_USER="CarlosArriaga"
-export KBUILD_BUILD_HOST="EroticHosting"
+export KBUILD_BUILD_USER="TheStrechh"
+export KBUILD_BUILD_HOST="legacyhost"
 export ALLOW_MISSING_DEPENDENCIES=true
 
 # Make a clean build, building dirty after you have had jack issues may result in a failed build
@@ -33,14 +24,8 @@ make clean && make clobber
 
 # Set CCACHE
 export USE_CCACHE=1
-export CCACHE_DIR=/root/ccache/lineage
+export CCACHE_DIR=/home/university/ccache/aex
 prebuilts/misc/linux-x86/ccache/ccache -M 80G
-
-# FuckingJack
-./prebuilts/sdk/tools/jack-admin kill-server
-export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx10g"
-./prebuilts/sdk/tools/jack-admin start-server
-export LANGUAGE=C
 
 # Check the starting time (of the real build process)
 TIME_START=$(date +%s.%N)
@@ -52,7 +37,7 @@ echo -e ""
 # Compile the build
 . build/envsetup.sh
 lunch lineage_$device-userdebug
-make bacon -j32
+make bacon -j4
 
 # Check the finishing time
 TIME_END=$(date +%s.%N)
