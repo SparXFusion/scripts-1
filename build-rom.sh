8f75f2413b3055be69a376a544fbf21149fc8354
Carlos Arriaga <37526095+TheStrechh@users.noreply.github.com>
2018-04-27 18:34:26 -0500

    Update build-rom.sh

--- build-rom.sh
+++ build-rom.sh
@@ -7,23 +7,18 @@
 rom=lineage # The rom name
 username=prada # Your user name
 
-# Colors makes things beautiful
-red=$(tput setaf 1)             #  red
-grn=$(tput setaf 2)             #  green
-blu=$(tput setaf 4)             #  blue
-cya=$(tput setaf 6)             #  cyan
-txtrst=$(tput sgr0)             #  Reset
-
 # CCACHE UMMM!!! Cooks my builds fast
-echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
+echo -e "CCACHE is enabled for this build"
 export USE_CCACHE=1
 export CCACHE_DIR=/home/$username/ccache/$rom
 prebuilts/misc/linux-x86/ccache/ccache -M 50G
 
 # Clean build
+echo -e "Cleaned SHIT"
 make clean && make clobber
 
 # Build ROM
+echo -e "BUILDING ROM :D"
 . build/envsetup.sh
 lunch lineage_kenzo-userdebug
 make bacon -j8
